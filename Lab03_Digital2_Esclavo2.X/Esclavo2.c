e7577de0568b47bbb29f0ce906ca77c85bd439b8
Blackdead1521g <125511148+Blackdead1521g@users.noreply.github.com>
2023-08-04 23:17:50 -0600

    Add files via upload

---
+++ Lab03_Digital2_Esclavo2.X/Esclavo2.c
@@ -0,0 +1,150 @@
+//*****************************************************************************
+/*
+ * File:   main.c
+ * Author: Pablo
+ * Ejemplo de implementación de la comunicación SPI 
+ * Código Esclavo
+ * Created on 10 de febrero de 2020, 03:32 PM
+ */
+//*****************************************************************************
+//*****************************************************************************
+// Palabra de configuración
+//*****************************************************************************
+// CONFIG1
+#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (RCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, RC on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+//*****************************************************************************
+// Definición e importación de librerías
+//*****************************************************************************
+#include <xc.h>
+#include <stdint.h>
+#include "SPI.h"
+#include "ADC.h"
+//*****************************************************************************
+// Definición de variables
+//*****************************************************************************
+#define _XTAL_FREQ 8000000
+#define cont 0
+
+uint8_t temporal = 0;
+int POT2 = 0;
+volatile uint8_t contador = 0;
+int recibido = 0;
+volatile uint8_t valor = 0;
+//*****************************************************************************
+// Definición de funciones para que se puedan colocar después del main de lo 
+// contrario hay que colocarlos todas las funciones antes del main
+//*****************************************************************************
+void setup(void);
+//*****************************************************************************
+// Código de Interrupción 
+//*****************************************************************************
+void __interrupt() isr(void){
+    //Interrupción del ADC
+    if (PIR1bits.ADIF) {
+        POT2 = ADRESH;//adc_read(); //Le asignamos a número el valor del ADC
+        
+        PIR1bits.ADIF = 0;
+    }
+    
+    if(INTCONbits.RBIF){
+        if(PORTBbits.RB1 == 0){//Incrementa contador
+            contador++;
+        }
+        else if(PORTBbits.RB0 == 0){ //Decrementa contador
+            contador--;
+        }
+        INTCONbits.RBIF = 0;                                    //Limpia la bandera de interrupción
+    
+    }
+    
+    if(PIR1bits.SSPIF == 1){ //Revisamos si ya hay datos que recibir
+        valor = spiRead();
+        if (valor == 0) //Si el dato recibido es 0 mandamos el valor del ADC
+        {
+            spiWrite(POT2);
+        }
+        if (valor == 1) //Si el dato recibido es 1 mandamos el valor del contador
+        {
+            spiWrite(contador);
+            //__delay_ms(1);
+        }
+        PIR1bits.SSPIF = 0;
+    }
+    
+    
+}
+//*****************************************************************************
+// Código Principal
+//*****************************************************************************
+void main(void) {
+    setup();
+    //*************************************************************************
+    // Loop infinito
+    //*************************************************************************
+    while(1){
+       //PORTB = numero;
+        
+        PORTD = contador;
+        
+       if (ADCON0bits.GO == 0) { // Si la lectura del ADC se desactiva
+
+            __delay_us(1000); //Este es el tiempo que se dará cada vez que se desactiva la lectura
+            ADCON0bits.GO = 1; //Activamos la lectura del ADC
+        }
+       __delay_ms(250);
+    }
+    return;
+}
+//*****************************************************************************
+// Función de Inicialización
+//*****************************************************************************
+void setup(void){
+    adc_init(0);
+    //definir digitales
+    ANSEL = 0;
+    ANSELbits.ANS0 = 1; //Seleccionamos solo los dos pines que utilizaremos como analógicos
+    ANSELH = 0;
+    
+    //Definimos puertos que serán salidas
+    TRISB = 0b11111111;
+    TRISD = 0;
+    TRISA = 0b11111111;
+    TRISCbits.TRISC3 = 1;
+    
+    PORTB = 0;
+    PORTD = 0;
+    
+    OPTION_REGbits.nRBPU = 0; //Habilitamos los PULLUPS
+    IOCB = 0b11111111; //Habilitamos las interrupciones al cambiar de estaso el puerto B
+    
+    OSCCONbits.IRCF = 0b111; //8 MHz
+    OSCCONbits.SCS = 1;
+    
+    INTCONbits.RBIF = 0; //Apagamos la bandera del puerto B
+    INTCONbits.RBIE = 1; //Habilitamos la interrupción en el puerto B
+    INTCONbits.GIE = 1;         // Habilitamos interrupciones
+    INTCONbits.PEIE = 1;        // Habilitamos interrupciones PEIE
+    PIR1bits.SSPIF = 0;         // Borramos bandera interrupción MSSP
+    PIE1bits.SSPIE = 1;         // Habilitamos interrupción MSSP
+    TRISAbits.TRISA5 = 1;       // Slave Select
+    spiInit(SPI_SLAVE_SS_EN, SPI_DATA_SAMPLE_MIDDLE, SPI_CLOCK_IDLE_LOW, SPI_IDLE_2_ACTIVE);
+    return;
+}
+
